40d33fe50c9908298dfe82ebdf90bafaba66bb86
WeslBaker <wesley.baker@students.fhu.edu>
2022-10-10 20:32:52 -0500

    My changes from claass

--- PracticeExercise2/LinkedList.cs
+++ PracticeExercise2/LinkedList.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+
 namespace PracticeExercise2
 {
 
@@ -37,7 +39,8 @@ namespace PracticeExercise2
 
         public bool IsEmpty => throw new NotImplementedException();
 
-        public int Length => throw new NotImplementedException();
+        private int length = 0;
+        public int Length => length;
 
         public void Append(T value)
         {
@@ -55,12 +58,14 @@ namespace PracticeExercise2
                 Tail = newNode;
             }
 
-
+            length++;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            Head = null;
+            Tail = null;
+            length = 0;
         }
 
         public bool Contains(T value)
@@ -96,31 +101,97 @@ namespace PracticeExercise2
         public void InsertAfter(T newValue, int existingValue)
         {
             throw new NotImplementedException();
+            length++;
         }
 
         public void InsertAt(T value, int index)
         {
             throw new NotImplementedException();
+            length++;
         }
 
         public void Prepend(T value)
         {
             throw new NotImplementedException();
+            length++;
         }
 
         public void Remove(T value)
         {
-            throw new NotImplementedException();
+            //Handle an empty list
+            if(IsEmpty)
+            {
+                return;
+            }
+
+            //If removing the head
+            if(Head.Data.Equals(value))
+            {
+                //1-element list
+                if (Head == Tail)
+                {
+                    Tail = null;
+                    Head = null;
+                }
+                else
+                {
+                Head = Head.Next;
+                }
+                length--;
+                return;
+            }
+
+            var currentNode = Head;
+            while(currentNode != null)
+            {
+                if(currentNode.Next != null && currentNode.Next.Data.Equals(value))
+                {
+                    var nodeToDelete = currentNode.Next;
+                    length--;
+                    if(nodeToDelete == Tail)
+                    {
+                        currentNode.Next = null;
+                        Tail = currentNode;
+        }
+                    else
+                    {
+                        currentNode.Next = currentNode.Next.Next;
+                        nodeToDelete.Next = null;
+                    }
+                    return;
+                }
+                currentNode = currentNode.Next;
+            }
         }
 
         public void RemoveAt(int index)
         {
             throw new NotImplementedException();
+            length--;
+
         }
 
         public IList<T> Reverse()
         {
             throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            string result = "[ ";
+
+            for(var currentNode = Head; currentNode != null; currentNode = currentNode.Next)
+            {
+                if (currentNode != Tail)
+                {
+                    result += $"{currentNode.ToString()}, ";
+                }
+                else
+                {
+                    result += $"{currentNode.ToString()} ]";
+                }
+            }
+            return result;
         }
     }
 }
